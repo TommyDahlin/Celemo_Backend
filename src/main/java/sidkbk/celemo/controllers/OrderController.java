0a9647a64c7f1a85a5abe82a13e17a860afc409e
Mezsoa <johnmessoa@gmail.com>
2024-02-16 23:18:59 +0100

    created GETmapping for all orders

--- src/main/java/sidkbk/celemo/controllers/OrderController.java
+++ src/main/java/sidkbk/celemo/controllers/OrderController.java
@@ -1,4 +1,29 @@
 package sidkbk.celemo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import sidkbk.celemo.models.Order;
+import sidkbk.celemo.services.OrderService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/order")
 public class OrderController {
+
+    @Autowired
+    OrderService orderService;
+
+    @PostMapping("/add")
+    public Order createOrder(@RequestBody Order order) {
+        return orderService.createOrder(order);
+    }
+
+    @GetMapping("/find")
+    public List<Order> getAllOrders() {
+        return orderService.getAllOrders();
+    }
+
+
 }
+
