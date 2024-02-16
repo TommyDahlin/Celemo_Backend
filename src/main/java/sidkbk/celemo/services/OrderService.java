73b23fda2859c079c4efebcf6de25e3f7916c929
john <johnmessoa@gmail.com>
2024-02-16 14:10:57 +0100

    creating the logic for orderservices

--- src/main/java/sidkbk/celemo/services/OrderService.java
+++ src/main/java/sidkbk/celemo/services/OrderService.java
@@ -2,10 +2,16 @@ package sidkbk.celemo.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import sidkbk.celemo.models.Order;
+import sidkbk.celemo.repository.OrderRepository;
 
 @Service
 public class OrderService {
 
     @Autowired
-    OrderService orderService
+    OrderRepository orderRepository;
+
+    public Order createOrder(Order order) {
+        return orderRepository.save(order);
+    }
 }
