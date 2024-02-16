a163290cc2dc550bb376a6c189cb1dee98fd2ac9
Tommy Dahlin <tommy.dahlin@studerande.plushogskolan.se>
2024-02-16 08:38:06 +0100

    Initialize Spring boot with some of the needed dependencies to start the project

---
+++ src/main/java/sidkbk/celemo/CelemoApplication.java
@@ -0,0 +1,13 @@
+package sidkbk.celemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CelemoApplication.class, args);
+	}
+
+}
