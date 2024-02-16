69c5c9126dde87303ce985d78f21ff78369eb5e8
john <johnmessoa@gmail.com>
2024-02-16 12:06:28 +0100

    models order, getters and setters, calc on commision

--- src/main/java/sidkbk/celemo/models/Order.java
+++ src/main/java/sidkbk/celemo/models/Order.java
@@ -1,4 +1,75 @@
 package sidkbk.celemo.models;
 
+import org.apache.catalina.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "orders")
 public class Order {
+
+    @Id
+    private String id;
+    @DBRef
+    private String sellerId;
+    @DBRef
+    private String buyerId;
+    private String productTitle;
+    @DBRef
+    private int endPrice;
+
+    //calculating total ammount for the commission of 3%
+    public double getTotalAmountCommission() {
+        double commission = 0.3 * endPrice;
+        double totalAmount = endPrice + commission;
+                return totalAmount;
+    }
+
+
+    public Order() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSellerId() {
+        return sellerId;
+    }
+
+    public void setSellerId(String sellerId) {
+        this.sellerId = sellerId;
+    }
+
+    public String getBuyerId() {
+        return buyerId;
+    }
+
+    public void setBuyerId(String buyerId) {
+        this.buyerId = buyerId;
+    }
+
+    public String getProductTitle() {
+        return productTitle;
+    }
+
+    public void setProductTitle(String productTitle) {
+        this.productTitle = productTitle;
+    }
+
+    public int getEndPrice() {
+        return endPrice;
+    }
+
+    public void setEndPrice(int endPrice) {
+        this.endPrice = endPrice;
+    }
+
+
+
 }
