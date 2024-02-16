f9e42b4b02e40ac240b242632f649e23f7ab7edf
Mezsoa <johnmessoa@gmail.com>
2024-02-16 14:22:28 +0100

    Done creating logic in orderService

--- src/main/java/sidkbk/celemo/services/OrderService.java
+++ src/main/java/sidkbk/celemo/services/OrderService.java
@@ -5,13 +5,39 @@ import org.springframework.stereotype.Service;
 import sidkbk.celemo.models.Order;
 import sidkbk.celemo.repository.OrderRepository;
 
+import java.util.List;
+
 @Service
 public class OrderService {
 
     @Autowired
     OrderRepository orderRepository;
 
+
+    // Create an order
     public Order createOrder(Order order) {
         return orderRepository.save(order);
     }
+
+    // READ ALL ORDERS
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+
+    //READ 1 ORDER
+    public Order getOneOrder(String id) {
+        return orderRepository.findById(id).get();
+    }
+
+    // PUT update one order
+    public Order updateOrder(Order order) {
+        return orderRepository.save(order);
+    }
+
+    // Delete one order by id
+    public String deleteOrder(String id) {
+        orderRepository.deleteById(id);
+        return "Deleted order successfully!";
+    }
+
 }
