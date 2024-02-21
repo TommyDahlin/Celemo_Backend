aaee4dda74a40388803a211c12d1842725fa48e3
Tommy Dahlin <tommy.dahlin@studerande.plushogskolan.se>
2024-02-21 10:19:41 +0100

    BRANCH IS FINISHED.

--- src/main/java/sidkbk/celemo/models/User.java
+++ src/main/java/sidkbk/celemo/models/User.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.ArrayList;
 
-@Document(collection = "account")
+@Document(collection = "users")
 public class User {
 
     @Id
