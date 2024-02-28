8022706fb77bafe76ebcd207b79fd8c0c3006805
Viktor Nyberg <viktor.nyberg00@gmail.com>
2024-02-28 17:22:34 +0100

    notBlank putDTO

--- src/main/java/sidkbk/celemo/dto/Reviews/ReviewsPutDTO.java
+++ src/main/java/sidkbk/celemo/dto/Reviews/ReviewsPutDTO.java
@@ -1,7 +1,10 @@
 package sidkbk.celemo.dto.Reviews;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class ReviewsPutDTO {
 
+    @NotBlank
     private String reviewId;
 
     private Double grade;
