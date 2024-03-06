a7aee69d64a5bcde60bb4dc851ded59fbe730095
Johan Johnsson <johan@johnsson-net.se>
2024-03-06 11:48:27 +0100

    missing component

--- src/main/java/sidkbk/celemo/security/jwt/JwtUtils.java
+++ src/main/java/sidkbk/celemo/security/jwt/JwtUtils.java
@@ -9,12 +9,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
 import org.springframework.web.util.WebUtils;
 import sidkbk.celemo.security.services.UserDetailsImpl;
 
 import java.security.Key;
 import java.util.Date;
 
+@Component
 public class JwtUtils {
     private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
