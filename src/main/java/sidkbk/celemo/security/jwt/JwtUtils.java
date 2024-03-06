0bd00eb1452366bd1e0651bf0c5354028e368a6e
Tommy Dahlin <tommy.dahlin@studerande.plushogskolan.se>
2024-03-06 11:15:12 +0100

    JwtUtils added

--- src/main/java/sidkbk/celemo/security/jwt/JwtUtils.java
+++ src/main/java/sidkbk/celemo/security/jwt/JwtUtils.java
@@ -9,6 +9,9 @@ import org.springframework.http.ResponseCookie;
 import org.springframework.web.util.WebUtils;
 import sidkbk.celemo.security.services.UserDetailsImpl;
 
+import java.security.Key;
+import java.util.Date;
+
 public class JwtUtils {
     private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
@@ -37,4 +40,35 @@ public class JwtUtils {
         ResponseCookie cookie = ResponseCookie.from(jwtCookie, null).path("/api").build();
         return cookie;
     }
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(key()).build()
+                .parseClaimsJws(token).getBody().getSubject();
+    }
+    private Key key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key()).build().parse(authToken);
+            return true;
+        } catch (MalformedJwtException e) {
+            logger.error("Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.error("JWT token has expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT claim string is empty: {}", e.getMessage());
+        }
+        return false;
+    }
+    public String generateTokenFromUsername(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(key(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
 }
