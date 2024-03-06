a192d3c5eb05c5b7c814a1662e2d2dd197cc8e98
Viktor Nyberg <viktor.nyberg00@gmail.com>
2024-03-06 11:20:24 +0100

    authController

--- src/main/java/sidkbk/celemo/controllers/AuthController.java
+++ src/main/java/sidkbk/celemo/controllers/AuthController.java
@@ -2,6 +2,7 @@ package sidkbk.celemo.controllers;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,13 +11,22 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import sidkbk.celemo.models.ERole;
+import sidkbk.celemo.models.Role;
+import sidkbk.celemo.models.User;
 import sidkbk.celemo.payload.request.SigninRequest;
+import sidkbk.celemo.payload.request.SignupRequest;
+import sidkbk.celemo.payload.response.MessageResponse;
+import sidkbk.celemo.payload.response.UserInfoResponse;
 import sidkbk.celemo.repositories.RoleRepository;
 import sidkbk.celemo.repositories.UserRepository;
 import sidkbk.celemo.security.jwt.JwtUtils;
 import sidkbk.celemo.security.services.UserDetailsImpl;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -46,8 +56,62 @@ public class AuthController {
 
         // jwt in cookie
         ResponseCookie jwtCookie = jwtUtils.generateJwtCookie(userDetails);
-        List<String> roles = userDetails.getAuthorities()
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
+                .body(new UserInfoResponse(userDetails.getId(),
+                        userDetails.getUsername(),
+                        userDetails.getEmail(),
+                        roles));
     }
+
+    // sign up/register new user
+    @PostMapping("/signup")
+    public ResponseEntity<?> signupUser(@Valid @RequestBody SignupRequest signupRequest){
+        if (userRepository.existsByUsername((signupRequest.getUsername()))) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: username already exists!"));
+        }
+
+        // create user account
+
+        User user = new User(signupRequest.getUsername(),
+                signupRequest.getEmail(),
+                encoder.encode(signupRequest.getPassword()));
+
+        Set<String> strRoles = signupRequest.getRoles();
+        Set<Role> roles = new HashSet<>();
+
+        if (strRoles == null) {
+            Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                    .orElseThrow(() -> new RuntimeException("Error: roles is not found"));
+            roles.add(userRole);
+        } else {
+            strRoles.forEach(role -> {
+                switch (role) {
+                    case "admin" -> {
+                        Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+                                .orElseThrow(() -> new RuntimeException("Error: Role not found"));
+                        roles.add(adminRole);
+                    }
+                    default -> {
+                        Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                                .orElseThrow(() -> new RuntimeException("Error: Role not found"));
+                        roles.add(userRole);
+                    }
+                }
+            });
+        }
+        user.setRoles(roles);
+        userRepository.save(user);
+
+        return ResponseEntity.ok(new MessageResponse("User registered successfully"));
+    }
+
+
 
 
 }
