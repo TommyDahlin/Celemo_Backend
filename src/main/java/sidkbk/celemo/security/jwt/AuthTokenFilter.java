bc182a1393554659664cc66e6eeec0761939ded3
Tommy Dahlin <tommy.dahlin@studerande.plushogskolan.se>
2024-03-06 11:31:37 +0100

    userdetailsServiceimpl

--- src/main/java/sidkbk/celemo/security/jwt/AuthTokenFilter.java
+++ src/main/java/sidkbk/celemo/security/jwt/AuthTokenFilter.java
@@ -33,7 +33,7 @@ public class AuthTokenFilter extends OncePerRequestFilter {
             if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
                 String username = jwtUtils.getUserNameFromJwtToken(jwt);
 
-                UserDetails userDetails =userDetailsService.loadUserByUsername(username);
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
                 UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                 authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authentication);
