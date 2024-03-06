fc456009fc908f94770ddd234eee20a7b1781fba
Tommy Dahlin <tommy.dahlin@studerande.plushogskolan.se>
2024-03-06 11:36:46 +0100

    lesgo

--- src/main/java/sidkbk/celemo/security/services/UserDetailsImpl.java
+++ src/main/java/sidkbk/celemo/security/services/UserDetailsImpl.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import sidkbk.celemo.models.User;
 
 import java.util.Collection;
 import java.util.List;
