1583c659f548e940d8989e7b6c5fa056cfd150ee
Viktor Nyberg <viktor.nyberg00@gmail.com>
2024-03-07 17:13:37 +0100

    removed function in repository with no function

--- src/main/java/sidkbk/celemo/repositories/UserRepository.java
+++ src/main/java/sidkbk/celemo/repositories/UserRepository.java
@@ -1,14 +1,10 @@
 package sidkbk.celemo.repositories;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 import sidkbk.celemo.models.User;
 
-import java.util.List;
 
-@Repository
 public interface UserRepository extends MongoRepository<User, String> {
 
-    //find all users and return username and email
-    List<User> findAllById(String id);
+
 }
