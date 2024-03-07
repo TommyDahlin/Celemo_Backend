b717fab24651b160b97238500c53fcb6b67be8f0
Johan Johnsson <johan@johnsson-net.se>
2024-03-07 13:16:49 +0100

    AuctionController

--- src/main/java/sidkbk/celemo/controllers/AuctionController.java
+++ src/main/java/sidkbk/celemo/controllers/AuctionController.java
@@ -12,22 +12,14 @@ import sidkbk.celemo.exceptions.EntityNotFoundException;
 import sidkbk.celemo.services.AuctionService;
 
 @RestController
-@RequestMapping(value = "/api/auction")
+@RequestMapping(value = "/auction")
 public class AuctionController {
 
     @Autowired
     AuctionService auctionService;
 
-    // POST create new auction
-    @PostMapping("/post")
-    public ResponseEntity<?> createAuction(@Valid @RequestBody AuctionCreationDTO auctionCreationDTO) {
-        try{
-            return ResponseEntity.ok(auctionService.createAuction(auctionCreationDTO));
-        }catch (EntityNotFoundException e) {
-            return  ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        }
-    }
-
+// PUBLIC
+//////////////////////////////////////////////////////////////////////////////////////
 
     // Get all auctions
     @GetMapping("/find/all")
@@ -39,8 +31,40 @@ public class AuctionController {
         }
     }
 
+// USER
+//////////////////////////////////////////////////////////////////////////////////////
+
+    // POST create new auction
+    @PostMapping("/create")
+    public ResponseEntity<?> createAuction(@Valid @RequestBody AuctionCreationDTO auctionCreationDTO) {
+        try{
+            return ResponseEntity.ok(auctionService.createAuction(auctionCreationDTO));
+        }catch (EntityNotFoundException e) {
+            return  ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    // PUT update an auction
+    @PutMapping("/update")
+    public ResponseEntity<?> updateAuction(@Valid @RequestBody AuctionUpdateDTO auctionUpdateDTO) {
+        try {
+            return ResponseEntity.ok(auctionService.updateAuction(auctionUpdateDTO));
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    // DELETE an auction
+    @DeleteMapping("/delete")
+    public ResponseEntity<?> deleteAuction(@Valid @RequestBody AuctionIdDTO auctionIdDTO) {
+        return auctionService.deleteAuction(auctionIdDTO);
+    }
+
+// ADMIN
+//////////////////////////////////////////////////////////////////////////////////////
+
     // GET one auction
-    @GetMapping("/find")
+    @GetMapping("/find-one")
     public ResponseEntity<?> getAuction(@Valid @RequestBody AuctionIdDTO auctionIdDTO) {
         try {
             return ResponseEntity.ok(auctionService.getOneAuction(auctionIdDTO));
@@ -49,23 +73,8 @@ public class AuctionController {
         }
     }
 
-    // PUT update an auction
-    @PutMapping("/put")
-    public ResponseEntity<?> updateAuction(@Valid @RequestBody AuctionUpdateDTO auctionUpdateDTO) {
-        try {
-            return ResponseEntity.ok(auctionService.updateAuction(auctionUpdateDTO));
-        } catch (EntityNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        }
-    }
-    // DELETE an auction
-    @DeleteMapping("/delete")
-    public ResponseEntity<?> deleteAuction(@Valid @RequestBody AuctionIdDTO auctionIdDTO) {
-        return auctionService.deleteAuction(auctionIdDTO);
-    }
-
     //REMOVE BEFORE PRODUCTION
-    @DeleteMapping("/deleteAll")
+    @DeleteMapping("/dev/deleteAll")
     public void deleteAllAuctions(){
         auctionService.deleteAllAuctions();
     }
