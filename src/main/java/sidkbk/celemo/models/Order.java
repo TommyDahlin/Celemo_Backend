a2a1bac04ded374102b39d2d3965f664052caccc
MattiYaqoob3 <matti.al-hayali@studerande.plushogskolan.se>
2024-09-12 13:05:52 +0200

    new model

--- src/main/java/sidkbk/celemo/models/Order.java
+++ src/main/java/sidkbk/celemo/models/Order.java
@@ -1,87 +1,35 @@
 package sidkbk.celemo.models;
 
-import org.springframework.data.annotation.CreatedDate;
+import lombok.Builder;
+import lombok.Getter;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
-
 import java.util.Date;
 
+@Getter
+@Builder
 @Document(collection = "orders")
 public class Order {
 
+    // Variables
     @Id
     private String id;
 
+    private String auctionId;
+
+    private String sellerId;
+
+    private String sellerFullName;
+
+    private String buyerID;
+
+    private String buyerFullName;
+
+    private Date createdDate;
+
     private String productTitle;
-    private double endPrice;
-    @CreatedDate
-    private Date createdAt = new Date();
 
+    private Double endPrice;       //endPrice * 0.97
 
-    private String sellerUsername;
-
-    private String buyerUsername;
-    @DBRef
-    private Auction auction; // this could be a problem
-
-
-
-
-
-
-
-
-    public Order() {
-    }
-
-
-
-    public String getId() {
-        return id;
-    }
-    public String getSellerUsername() {
-        return sellerUsername;
-    }
-    public String getBuyerUsername() {
-        return buyerUsername;
-    }
-    public Auction getAuction() {
-        return auction;
-    }
-    public String getProductTitle() {
-        return productTitle;
-    }
-    public double getEndPrice() {
-        return endPrice;
-    }
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-
-
-
-
-    public void setEndPrice(double endPrice) {
-        this.endPrice = endPrice;
-    }
-    public void setAuction(Auction auction) {
-        this.auction = auction;
-    }
-    public void setBuyerUsername(String buyerUsername) {
-        this.buyerUsername = buyerUsername;
-    }
-    public void setSellerUsername(String sellerUsername) {
-        this.sellerUsername = sellerUsername;
-    }
-    public void setProductTitle(String productTitle) {
-        this.productTitle = productTitle;
-    }
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-    public void setId(String id) {
-        this.id = id;
-    }
+    private Double commission;   // endPrice * 0.03
 }
