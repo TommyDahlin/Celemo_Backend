42983ee631469c1fe20c63b8458d78ef3bbc0a2d
Johan Johnsson <johan@johnsson-net.se>
2024-09-16 13:44:06 +0200

    spring scheduler

--- src/main/java/sidkbk/celemo/CelemoApplication.java
+++ src/main/java/sidkbk/celemo/CelemoApplication.java
@@ -3,15 +3,15 @@ package sidkbk.celemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
-import sidkbk.celemo.services.TimerService;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableMongoAuditing
+@EnableScheduling
 public class CelemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CelemoApplication.class, args);
-		TimerService.checkAuctionEndTimeTimer();
 	}
 
 }
--- src/main/java/sidkbk/celemo/services/TimerService.java
+++ src/main/java/sidkbk/celemo/services/TimerService.java
@@ -2,33 +2,27 @@ package sidkbk.celemo.services;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import sidkbk.celemo.dto.order.OrderCreationDTO;
 import sidkbk.celemo.models.Auction;
 import sidkbk.celemo.repositories.AuctionRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 @Service
 public class TimerService {
 
     @Autowired
-    static AuctionRepository auctionRepository;
+    AuctionRepository auctionRepository;
 
-    public static void checkAuctionEndTimeTimer() {
-        ScheduledExecutorService auctionTimer = new ScheduledThreadPoolExecutor(2);
-        auctionTimer.scheduleWithFixedDelay(TimerService::checkAuctionEndTime,10 , 10, TimeUnit.SECONDS);
-    }
-
-    public static void checkAuctionEndTime() {
+    @Scheduled(fixedDelay = 5000, initialDelay = 5 * 1000)
+    public void checkAuctionEndTime() {
         List<Auction> allAuctions = auctionRepository.findAll();
         for (Auction auction : allAuctions) {
-            if (auction.getEndDate().isBefore(LocalDateTime.now())) {
+            if (auction.getEndDate().isBefore(LocalDateTime.now()) && !auction.isFinished) {
                 auction.setFinished(true);
-                System.out.println("an acution set to finished");
+                auctionRepository.save(auction);
+                System.out.println(auction.getId() + " set to finished");
             }
         }
     }
